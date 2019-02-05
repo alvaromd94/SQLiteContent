ea90bfa0bcc0be9329f5da3092cca1a362573f76
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-02-05 09:36:44 +0100

    Initial commit

---
+++ app/src/main/java/com/alvar/sqlitecontent/DataBaseManager/DB_SQLite.java
@@ -0,0 +1,39 @@
+package com.alvar.sqlitecontent.DataBaseManager;
+
+import com.alvar.sqlitecontent.DataBaseManager.Esquema.Producto;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DB_SQLite extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "tienda.sqlite";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + Producto.TABLE_NAME + " (" +
+                    Producto.COLUMN_NAME_ID + " " + Producto.COLUMN_TYPE_ID + " PRIMARY KEY AUTOINCREMENT, " +
+                    Producto.COLUMN_NAME_NOMBRE + " " + Producto.COLUMN_TYPE_NOMBRE + "," +
+                    Producto.COLUMN_NAME_CANTIDAD + " " + Producto.COLUMN_TYPE_CANTIDAD +  "," +
+                    Producto.COLUMN_NAME_SECCION + " " + Producto.COLUMN_TYPE_SECCION + ")";
+
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + Producto.TABLE_NAME;
+
+    public DB_SQLite(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+}
